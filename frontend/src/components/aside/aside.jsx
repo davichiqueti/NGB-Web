83a053eb97e85f36b79e6d4608a2718fade456ff
GustavoPAraujo <araujops.gustavo@gmail.com>
2024-10-01 16:06:15 -0300

    feat: sidebar eventlistener

--- frontend/src/components/aside/aside.jsx
+++ frontend/src/components/aside/aside.jsx
@@ -1,59 +1,75 @@
-import styles from './aside.module.css'
-
-import { Link } from 'react-router-dom'
-
+import React, { useState, useEffect } from 'react';
+import styles from './aside.module.css';
+import { Link } from 'react-router-dom';
 import { FaHashtag, FaHouse, FaBell, FaEnvelope } from "react-icons/fa6";
 import { MdPerson } from "react-icons/md";
 import { CgMoreO } from "react-icons/cg";
 
 export default function Aside() {
+    
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-    return(
-        <>
-
-            <aside className={styles.aside}>
-                <h1>Aside</h1>
-                <br />
+    const handleResize = () => {
+        setWindowWidth(window.innerWidth);
+    };
 
 
-                <Link to='/' className={styles.asideButton}>
-
-                    <FaHouse />
-                    <p>Home</p>  
-                </Link>
-
-                <Link to='/explore' className={styles.asideButton}>
-
-                    <FaHashtag />
-                    <p>Explore</p>  
-                </Link>
-
-                <Link to='/notifications' className={styles.asideButton}>
-
-                    <FaBell  />
-                    <p>Notifications</p>                
-                </Link>
-
-                <Link to='/messages' className={styles.asideButton}>
-
-                    <FaEnvelope />
-                    <p>Messages</p>
-                </Link>
-                
-                <Link to='/profile' className={styles.asideButton}>
-
-                    < MdPerson />
-                    <p>Profile</p>
-                </Link>
-
-                <Link to='/' className={styles.asideButton}>
-
-                    <CgMoreO />
-                    <p>More</p>
-                </Link>
-
-            </aside>
+    useEffect(() => {
         
-        </>
-    )
-};
+        window.addEventListener('resize', handleResize);
+
+        
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+
+    return (
+        <div>
+            {windowWidth > 640 ? (
+                <>
+                    <aside className={styles.aside}>
+                        <h1>Logo</h1>
+                        <br />
+
+                        <Link to='/' className={styles.asideButton}>
+                            <FaHouse />
+                            <p>Home</p>
+                        </Link>
+
+                        <Link to='/explore' className={styles.asideButton}>
+                            <FaHashtag />
+                            <p>Explore</p>
+                        </Link>
+
+                        <Link to='/notifications' className={styles.asideButton}>
+                            <FaBell />
+                            <p>Notifications</p>
+                        </Link>
+
+                        <Link to='/messages' className={styles.asideButton}>
+                            <FaEnvelope />
+                            <p>Messages</p>
+                        </Link>
+
+                        <Link to='/profile' className={styles.asideButton}>
+                            <MdPerson />
+                            <p>Profile</p>
+                        </Link>
+
+                        <Link to='/' className={styles.asideButton}>
+                            <CgMoreO />
+                            <p>More</p>
+                        </Link>
+                    </aside>
+                </>
+            ) : (
+                <>
+                
+                </>
+            )}
+
+        </div>
+    );
+}
