65f94f92b98237f85ff3c474f629080ec8dc4668
GustavoPAraujo <araujops.gustavo@gmail.com>
2024-10-01 15:22:50 -0300

    feat: aside links and icons

--- frontend/src/components/aside/aside.jsx
+++ frontend/src/components/aside/aside.jsx
@@ -1,5 +1,10 @@
 import styles from './aside.module.css'
 
+import { Link } from 'react-router-dom'
+
+import { FaHashtag, FaHouse, FaBell, FaEnvelope } from "react-icons/fa6";
+import { MdPerson } from "react-icons/md";
+import { CgMoreO } from "react-icons/cg";
 
 export default function Aside() {
 
@@ -9,11 +14,44 @@ export default function Aside() {
             <aside className={styles.aside}>
                 <h1>Aside</h1>
                 <br />
-                <p>Explore</p>
-                <p>Notifications</p>
-                <p>Messages</p>
-                <p>Profile</p>
-                <p>More</p>
+
+
+                <Link to='/' className={styles.asideButton}>
+
+                    <FaHouse />
+                    <p>Home</p>  
+                </Link>
+
+                <Link to='/explore' className={styles.asideButton}>
+
+                    <FaHashtag />
+                    <p>Explore</p>  
+                </Link>
+
+                <Link to='/notifications' className={styles.asideButton}>
+
+                    <FaBell  />
+                    <p>Notifications</p>                
+                </Link>
+
+                <Link to='/messages' className={styles.asideButton}>
+
+                    <FaEnvelope />
+                    <p>Messages</p>
+                </Link>
+                
+                <Link to='/profile' className={styles.asideButton}>
+
+                    < MdPerson />
+                    <p>Profile</p>
+                </Link>
+
+                <Link to='/' className={styles.asideButton}>
+
+                    <CgMoreO />
+                    <p>More</p>
+                </Link>
+
             </aside>
         
         </>
