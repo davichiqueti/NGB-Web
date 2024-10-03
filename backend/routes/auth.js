ad2f1de84960b696604ff2bc125b2dbf26582f3a
Davi Chiqueti <davichiqueti@gmail.com>
2024-10-03 17:26:08 -0300

    feat: added route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { signup, login, logout, authCheck } from "../controllers/auth.js";
+import { authCheck, signup, login, logout, deleteAccount } from "../controllers/auth.js";
 import protectRoute from "../middleware/protectRoute.js"
 
 
@@ -8,5 +8,6 @@ router.get("/authcheck", protectRoute, authCheck);
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
+router.delete("/delete-account", protectRoute, deleteAccount); // New route for account deletion
 
 export default router;
