e8d0afd649798a5fb744cffa475d8b47835940a9
GustavoPAraujo <araujops.gustavo@gmail.com>
2024-11-14 11:12:20 -0300

    config + feat: tailwind config

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,9 +1,13 @@
-import Navigation from '../components/aside/aside.js'
+import Navigation from '../components/navigation/aside/aside.js'
 
 export default function Home() {
   return (
     <>
       <Navigation />
+
+      <div className="flex items-center justify-center h-screen bg-blue-500">
+        <h1 className="text-4xl font-bold text-white">Hello, Tailwind CSS!</h1>
+      </div>
     </>
   );
 }
