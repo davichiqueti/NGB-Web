cc9d755531bb7f28ed39fdbbede73ec6594c7779
GustavoPAraujo <araujops.gustavo@gmail.com>
2024-11-15 14:29:34 -0300

    styles: tailwind responsivness config + aside styles/responsivness

--- frontend/src/components/navigation/aside/aside.js
+++ frontend/src/components/navigation/aside/aside.js
@@ -1,19 +1,19 @@
-'use client'
+
 
 import Link from 'next/link';
 import { FaHashtag, FaHouse, FaBell, FaEnvelope } from "react-icons/fa6";
-import { MdPerson } from "react-icons/md";
 import { IoPersonOutline } from "react-icons/io5";
 
 export default function Aside() {
     return (
-        <aside className="hidden left-0 top-0 h-full
-                            sm:block  md:w-56 lg:w-72 xl:w-80 
-                            border-r border-r-1 border-slate-400
-                            flex flex-col justify-items-end 
-                            py-8  space-y-4">
+        <aside className="hidden sm-500:block fixed left-0 top-0 h-full
+                            sm-500:w-24 sm:w-32 md:w-36 md-900:w-40 lg:w-56
+                            border-r border-slate-400
+                            flex flex-col
+                            py-8 space-y-3">
 
-            <div id="nav">
+            <div id="nav" className="flex flex-col items-center space-y-3">
+
                 <Link href='/'>
                     <img
                         src="/logo.png"
@@ -22,32 +22,40 @@ export default function Aside() {
                     />
                 </Link>
 
-                <div className="space-y-4 pl-10">
-                    <Link href="/" className="text-2xl font-semibold hover:text-gray-300 flex items-center space-x-2">
-                        <FaHouse className="text-xl" />
-                        <p className="hidden md:block text-right">Home</p>
-                    </Link>
+                <Link href="/" className="text-2xl font-semibold hover:text-gray-300 flex items-center space-x-2">
+                    <FaHouse className="text-2xl" />
+                    <p className="text-left sm-500:hidden lg:block ">Home</p>
+                </Link>
 
-                    <Link href="/explore" className="text-2xl font-semibold hover:text-gray-300 flex items-center space-x-2">
-                        <FaHashtag className="text-xl" />
-                        <p className="hidden md:block text-left">Explore</p>
-                    </Link>
+                <Link href="/notifications" className="text-2xl font-semibold hover:text-gray-300 flex items-center space-x-2">
+                    <FaBell className="text-2xl" />
+                    <p className="text-left sm-500:hidden lg:block">Notifications</p>
+                </Link>
 
-                    <Link href="/notifications" className="text-2xl font-semibold hover:text-gray-300 flex items-center space-x-2">
-                        <FaBell className="text-xl" />
-                        <p className="hidden md:block text-left">Notifications</p>
-                    </Link>
 
-                    <Link href="/messages" className="text-2xl font-semibold hover:text-gray-300 flex items-center space-x-2">
-                        <FaEnvelope className="text-xl" />
-                        <p className="hidden md:block text-left">Messages</p>
-                    </Link>
+                {/*
+                
+                <Link href="/explore" className="text-2xl font-semibold hover:text-gray-300 flex items-center space-x-2">
+                    <FaHashtag className="text-xl" />
+                    <p className="text-left">Explore</p>
+                </Link>
 
-                    <Link href="/profile" className="text-2xl font-semibold hover:text-gray-300 flex items-center space-x-2">
-                        <IoPersonOutline className="text-xl" />
-                        <p className="hidden md:block text-left">Profile</p>
-                    </Link>
-                </div>
+                <Link href="/notifications" className="text-2xl font-semibold hover:text-gray-300 flex items-center space-x-2">
+                    <FaBell className="text-xl" />
+                    <p className="text-left">Notifications</p>
+                </Link>
+
+                <Link href="/messages" className="text-2xl font-semibold hover:text-gray-300 flex items-center space-x-2">
+                    <FaEnvelope className="text-xl" />
+                    <p className="text-left">Messages</p>
+                </Link>
+
+                <Link href="/profile" className="text-2xl font-semibold hover:text-gray-300 flex items-center space-x-2">
+                    <IoPersonOutline className="text-xl" />
+                    <p className="text-left">Profile</p>
+                </Link>
+                */}
+
             </div>
         </aside>
     );
