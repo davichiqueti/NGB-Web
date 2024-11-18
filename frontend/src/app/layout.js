221c4511ea18d996aebae8e5b5a3364e1fde6264
GustavoPAraujo <araujops.gustavo@gmail.com>
2024-11-18 14:02:40 -0300

    feat: auth context

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -3,8 +3,20 @@ import '../styles/globals.css';
 import Aside from "../components/navigation/aside/aside.js";
 import Footer from "@/components/navigation/mobile/footer/footer.js";
 import Header from "@/components/navigation/mobile/header/header.js";
+import { useAuth } from '@/context/authContext';
+import { useRouter } from 'next/navigation';
 
 export default function RootLayout({ children }) {
+  const { user } = useAuth();
+  const router = useRouter();
+
+  if (!user) {
+    // Redireciona para a página de login se o usuário não estiver autenticado
+    router.push('/login');
+    return null; // Evita renderizar conteúdo até que o redirecionamento ocorra
+  }
+
+
   return (
     <html lang="en">
       <body className="bg-gray-800 text-white flex flex-col min-h-screen">
