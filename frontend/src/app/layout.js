db74cc76490b4bfa513459b3bd7f7cc7722ae6e9
GustavoPAraujo <araujops.gustavo@gmail.com>
2024-11-19 01:03:39 -0300

    fix: layout problems

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,62 +1,63 @@
 'use client';
 
+import '../styles/globals.css';
 import { useRouter, usePathname } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import { checkAuth } from '../../services/authService';
-import Header from '@/components/navigation/aside/aside';
-import Aside from '@/components/navigation/mobile/header/header';
-import Footer from '@/components/navigation/mobile/footer/footer';
+
+import Aside from "../components/navigation/aside/aside.js";
+import Footer from "@/components/navigation/mobile/footer/footer.js";
+import Header from "@/components/navigation/mobile/header/header.js";
 
 export default function RootLayout({ children }) {
   const router = useRouter();
-  const pathname = usePathname();
+  const pathname = usePathname(); // Rota atual
+  const [isAuthenticating, setIsAuthenticating] = useState(true);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
 
-  const publicRoutes = ['/login', '/signup'];
+  // Rotas públicas que não precisam de autenticação
+  const publicRoutes = ['/auth/login', '/auth/signup'];
 
   useEffect(() => {
-    const validateAuth = async () => {
+    const authenticate = async () => {
       if (publicRoutes.includes(pathname)) {
-        setIsLoading(false); 
+        setIsAuthenticating(false);
         return;
       }
 
-      try {
-        const authResult = await checkAuth();
-        if (authResult.isAuthenticated) {
-          setIsAuthenticated(true);
-        } else {
-          router.push('/login');
-        }
-      } catch (error) {
-        console.error('Erro na validação de autenticação:', error);
-        router.push('/login');
-      } finally {
-        setIsLoading(false);
+      const authResult = await checkAuth();
+      if (authResult.isAuthenticated) {
+        setIsAuthenticated(true);
+      } else {
+        router.push('/auth/login');
       }
+
+      setIsAuthenticating(false);
     };
 
-    validateAuth();
+    authenticate();
   }, [pathname, router]);
 
-  if (!isAuthenticated && !publicRoutes.includes(pathname)) {
-    return null;
-  }
+  const isPublicRoute = publicRoutes.includes(pathname);
 
   return (
     <html lang="en">
-
       <body className="bg-gray-800 text-white flex flex-col min-h-screen">
 
-        {!publicRoutes.includes(pathname) && <Header />}
+        {!isPublicRoute && <Header />}
+
         <div className="flex flex-grow">
-          {!publicRoutes.includes(pathname) && <Aside className="hidden sm-500:block" />}
-          <main className="flex-grow sm-500:ml-24 sm:ml-32 md:ml-36 md-900:ml-40 lg:ml-64 xl:ml-72 2xl:ml-80 p-4">
+          
+          {!isPublicRoute && <Aside className="hidden sm-500:block" />}
+
+          <main className={`flex-grow ${!isPublicRoute ? 'sm-500:ml-24 sm:ml-32 md:ml-36 md-900:ml-40 lg:ml-64 xl:ml-72 2xl:ml-80 p-4' : ''}`}>
             {children}
           </main>
+
         </div>
-        {!publicRoutes.includes(pathname) && <Footer />}
+
+        {!isPublicRoute && <Footer />}
+
       </body>
     </html>
   );
--- frontend/src/middleware.js
+++ frontend/src/middleware.js
@@ -1,25 +1,20 @@
 import { NextResponse } from 'next/server';
 
-export function middleware(req) {
+export default function middleware(req) {
   const token = req.cookies.get('jwt');
 
-  const publicRoutes = ['/login', '/signup'];
+  const publicRoutes = ['/auth/login', '/auth/signup'];
 
   if (publicRoutes.includes(req.nextUrl.pathname)) {
-    // Permite acesso a rotas públicas
     return NextResponse.next();
   }
 
   if (!token) {
-    // Redireciona para login se o token estiver ausente
-    return NextResponse.redirect(new URL('/login', req.url));
+    return NextResponse.redirect(new URL('/auth/login', req.url));
   }
-
-  // Permite acesso às rotas privadas
   return NextResponse.next();
 }
 
-// Configurar o middleware para aplicar a todas as rotas
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'], // Aplica a todas as rotas, exceto estáticas
+  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
 };
