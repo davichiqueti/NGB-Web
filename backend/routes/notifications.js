054258bc5a1f164df4fc2e4c75a9a92c04a91534
gomesgustavoo <gustavoformento05@gmail.com>
2024-11-30 17:24:46 -0300

    feat: notification routes

---
+++ backend/routes/notifications.js
@@ -0,0 +1,11 @@
+import express from "express";
+import protectRoute from "../middleware/protectRoute";
+ 
+const router = express.Router();
+
+router.get("/", protectRoute, getNotifications);
+router.delete("/", protectRoute, deleteNotifications);
+ 
+
+export default router;
+
