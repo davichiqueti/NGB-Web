99550f90219999cbbaef2a3bd6a842147a558fa3
gomesgustavoo <gustavoformento05@gmail.com>
2024-11-30 18:04:57 -0300

    feat: get notifications

---
+++ backend/controllers/notifications.js
@@ -0,0 +1,22 @@
+export const getNotifications = async (req, res) => {
+
+    try{
+        const userId = req.user._id;
+
+        const notifications = await Notification.find({to:userId}).populate({
+            path: "from",
+            select: "username profileImg"
+        });
+
+        await Notification.updateMany({to:userId}, {read:true});
+
+        res.status(200).json(notifications);
+    }   catch (error){
+        console.log("Error in getNotifications", error.message);
+        res.status(500).json({ error: "Internal Server Error"});
+    }
+};
+
+
+export const deleteNotifications = async (req, res) => {};
+
