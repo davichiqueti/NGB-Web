f0452448878763342c11ae22a3b891d383707e8e
Davi Chiqueti <davichiqueti@gmail.com>
2024-12-05 17:02:50 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,16 +15,28 @@ dotenv.config();
 // Creating Application
 const app = express();
 
+// Setting allowed origins to backend
+const allowedOrigins = [
+    'http://localhost:3000', // Local Development
+    process.env.FRONTEND_URL, // Frontend Production URL
+];
 app.use(cors({
-    origin: 'http://localhost:3000',// allows front requests
+    origin: (origin, callback) => {
+        // Allow requests from allowed origins or no origin (e.g., server-to-server)
+        if (allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     credentials: true,
-  }));
+}));
 
-  cloudinary.config({
+cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
-  });
+});
 
 // Adding middlewares
 app.use(express.json()); // Handles API requests with JSON body
